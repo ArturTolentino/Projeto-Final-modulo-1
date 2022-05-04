b9d3aea325bb7155c9a99150f1d49e62644a93d8
ArturTolentino <97901283+ArturTolentino@users.noreply.github.com>
2022-05-04 04:10:33 -0300

    Adicionando o JavaScript
    adicionando a navegação entre páginas.

---
+++ Explorador Externo/JavaScript/Script2.js
@@ -0,0 +1,22 @@
+function rota (){
+  let escolha = 0
+  while(escolha == 0) {
+   escolha=(prompt("Escolha sua rota \n 1 ou 2"))
+   Number(escolha)
+    if(escolha == 1){
+      window.location.href=("../html/gameOver2.html")
+      
+    }
+    else if(escolha == 2){
+      window.location.href=("../html/ExExterno3.html")
+    }
+
+    else{
+      alert("Você deve escolher a rota 1 ou 2 !")
+      escolha = 0
+    }
+
+  }
+  Number(escolha)
+}
+rota()
---
+++ Explorador de tuneis/JS/JavaScript.js
@@ -0,0 +1,22 @@
+function rota (){
+  let escolha = 0
+  while(escolha == 0) {
+   escolha=(prompt("Escolha sua rota \n 1 ou 2"))
+   Number(escolha)
+    if(escolha == 1){
+      window.location.href=("../html/gameOver1.html")
+      
+    }
+    else if(escolha == 2){
+      window.location.href=("../html/ExTuneis2.html")
+    }
+
+    else{
+      alert("Você deve escolher a rota 1 ou 2 !")
+      escolha = 0
+    }
+
+  }
+  Number(escolha)
+}
+rota()
